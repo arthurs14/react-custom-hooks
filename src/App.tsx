97b9f475133a63510e76b16d710453d266cd6466
arthurs14 <arthurcs426@gmail.com>
2023-02-17 22:50:27 -0800

    two separate states due to separate component renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,11 @@
-import { FormEvent, useState } from 'react';
-import FormContainer from './components/FormContainer';
-import useMultistepForm from './hooks/useMultistepForm';
-import UserForm from './forms/UserForm';
-import AddressForm from './forms/AddressForm';
-import AccountForm from './forms/AccountForm';
-import { FormData } from './types/FormData';
-
-const INITIAL_DATA: FormData = {
-  firstName: '',
-  lastName: '',
-  age: '',
-  street: '',
-  city: '',
-  state: '',
-  zip: '',
-  email: '',
-  password: '',
-};
+import Form from './components/Form';
 
 const App = () => {
-  const [data, setData] = useState<FormData>(INITIAL_DATA);
-
-  const updateFields = (fields: Partial<FormData>) => {
-    setData((prevState) => {
-      return { ...prevState, ...fields };
-    });
-  };
-
-  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
-    useMultistepForm([
-      <UserForm {...data} updateFields={updateFields} />,
-      <AddressForm {...data} updateFields={updateFields} />,
-      <AccountForm {...data} updateFields={updateFields} />,
-    ]);
-
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    if (!isLastStep) return next();
-    alert('You have finished!');
-  };
-
-  console.log(data);
-
   return (
-    <div>
-      <FormContainer>
-        <form onSubmit={onSubmit}>
-          <div style={{ position: 'absolute', top: '.5rem', right: '.5rem' }}>
-            {currentStepIndex + 1} / {steps.length}
-          </div>
-          {step}
-          <div
-            style={{
-              margin: '1rem',
-              display: 'flex',
-              gap: '.5rem',
-              justifyContent: 'flex-end',
-            }}
-          >
-            {!isFirstStep && (
-              <button type="button" onClick={back}>
-                Back
-              </button>
-            )}
-            <button type="submit">{isLastStep ? 'Finish' : 'Next'}</button>
-          </div>
-        </form>
-      </FormContainer>
-      <FormContainer>
-        <form onSubmit={onSubmit}>
-          <div style={{ position: 'absolute', top: '.5rem', right: '.5rem' }}>
-            {currentStepIndex + 1} / {steps.length}
-          </div>
-          {step}
-          <div
-            style={{
-              margin: '1rem',
-              display: 'flex',
-              gap: '.5rem',
-              justifyContent: 'flex-end',
-            }}
-          >
-            {!isFirstStep && (
-              <button type="button" onClick={back}>
-                Back
-              </button>
-            )}
-            <button type="submit">{isLastStep ? 'Finish' : 'Next'}</button>
-          </div>
-        </form>
-      </FormContainer>
-    </div>
+    <>
+      <Form />
+      <Form />
+    </>
   );
 };
 
